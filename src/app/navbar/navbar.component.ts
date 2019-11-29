8665537d64e6c28efac3a84271d1f4f8830504af
prinz <princekm005@gmail.com>
2019-11-30 00:06:25 +0530

    created navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() {
+    console.debug("constructor");
+  }
+
+  ngOnInit() {
+  }
+
+  onClick()
+  {
+    console.debug("hello");
+  }
+
+}
